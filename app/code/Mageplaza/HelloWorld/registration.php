932f539e561a59318a8adb0c995ea6c681485432
gairik61 <gairik61@gmail.com>
2019-03-28 14:48:35 +0530

    New module hello world

---
+++ app/code/Mageplaza/HelloWorld/registration.php
@@ -0,0 +1,4 @@
+<?php
+use \Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(\Magento\Framework\Component\ComponentRegistrar::MODULE, 'Mageplaza_HelloWorld', __DIR__);
+?>
